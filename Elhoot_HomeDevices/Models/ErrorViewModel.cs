1c746dcf398e0daeb45e8c40ac71c0bbb1cc641e
pc <pc@DESKTOP-EV6RFJ5>
2023-07-11 04:07:02 +0300

    Add project files.

---
+++ Elhoot_HomeDevices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Elhoot_HomeDevices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
